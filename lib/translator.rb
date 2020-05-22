7c0ff0ba45b0fc7de87cd93d25b45efe5a954c85
Jodi Croft <jodipruett@gmail.com>
2020-05-22 22:57:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,13 @@ def get_english_meaning(file_path, emoticon)
   emoticon_file.each do |emotion_name, keys_and_emoticons|
     keys_and_emoticons.each do |english_japanese, string_of_emoticons|
       english_japanese.each do |english, japanese|
-        japanese
+        if japanese[string_of_emoticons] == emoticon
+          puts emotion_name
+        else puts "Sorry, that emoticon does not exist"
+      end
+    end
   end
+end
 end
 
 
