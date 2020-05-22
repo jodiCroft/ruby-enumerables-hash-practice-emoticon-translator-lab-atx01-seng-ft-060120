f30e0fd03fa0a0326321e8433b73d5a16dfde188
Jodi Croft <jodipruett@gmail.com>
2020-05-22 19:49:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,9 @@ require "pry"
 
 def load_library(file)
   emoticons = YAML.load_file(file)
+  emoticons.each_with_object({}) do |(main_keys, values), final_hash|
+    main_keys.each do |main_key|
+      binding.pry
 end
 
 def get_japanese_emoticon
