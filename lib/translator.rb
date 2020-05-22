94fcd07dcdd66b8b3ccc1b42532392db67d3740a
Jodi Croft <jodipruett@gmail.com>
2020-05-22 19:50:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,10 @@ require "yaml"
 require "pry"
 
 def load_library(file)
-  emoticons = YAML.load_file(file)
-  file.each_with_object({}) do |(main_keys, values), final_hash|
+  emoticons_file = YAML.load_file(file)
+  emoticons_file.each_with_object({}) do |(main_keys, values), final_hash|
     main_keys.each do |main_key|
+
       binding.pry
 end
 
