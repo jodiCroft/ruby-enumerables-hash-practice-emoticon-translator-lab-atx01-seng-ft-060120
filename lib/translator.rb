58cb9a6558ab08f0da0c755c4a8251802bc371ee
Jodi Croft <jodipruett@gmail.com>
2020-05-22 22:16:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   meaning_of_emoticon = emoticon_file.each do |keys, values|
     values.each do |inner_keys, emoticons|
       if emoticon == emoticons[1]
-        puts inner_keys[0]
+        inner_keys[0]
       else puts "Sorry, that emoticon was not found"
       end
     end
