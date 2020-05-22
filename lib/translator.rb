8556fc90d856b64dcef47086dc2f0fae9ac63029
Jodi Croft <jodipruett@gmail.com>
2020-05-22 21:26:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,13 +7,14 @@ def load_library(file_path)
     if !final_hash[keys]
       final_hash[keys] = {}
     end
+    binding.pry
     values.each do |english_emoticon, japanese_emoticon|
       if !final_hash[keys][:english]
         final_hash[keys][:english] = english_emoticon
       end
       if !final_hash[keys][:japanese]
         final_hash[keys][:japanese] = japanese_emoticon
-        binding.pry
+        #binding.pry
       end
     end
   end
