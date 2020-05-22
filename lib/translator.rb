a7e7429a0aae722e45163353622dcee4b5f2c72a
Jodi Croft <jodipruett@gmail.com>
2020-05-22 19:53:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,8 +6,8 @@ def load_library(file)
   binding.pry
   emoticons_file.each_with_object({}) do |(main_keys, values), final_hash|
     main_keys.each do |main_key|
-
-
+end
+end
 end
 
 def get_japanese_emoticon
