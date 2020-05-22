92f14fa88a47b794eb8903adb8c908584f19330a
Jodi Croft <jodipruett@gmail.com>
2020-05-22 21:38:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,7 @@ require "pry"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
+  final_hash = {}
   emoticons.each_with_object({}) do |(keys, values), final_hash|
     if !final_hash[keys]
       final_hash[keys] = {:english=>values[0], :japanese=>values[1]}
